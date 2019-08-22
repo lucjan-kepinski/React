35628e923f452401d9833a0293288ead0b3a90ce
Lucjan <lucjan.kepinski@gmail.com>
2019-08-22 16:18:17 +0200

    sorting by votes added, event to caret icon for product component added

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,15 +1,23 @@
 
 class ProductList extends React.Component {
+    handleProductUpVote(productId) {
+    console.log(productId + ' was upvoted.');
+    }
     render() {
+    const products = Seed.products.sort((a, b) => (
+      b.votes - a.votes
+      ));
     const productComponents = Seed.products.map((product) => (
           <Product 
             key={'product-' + product.id}
+            id={product.id}
             title={product.title}
             description={product.description}
             url={product.url}
             votes={product.votes}
             submitterAvatarUrl={product.submitterAvatarUrl}
             productImageUrl={product.productImageUrl}
+            onVote={this.handleProductUpVote}
           />
     ))
     return (
@@ -20,6 +28,13 @@ class ProductList extends React.Component {
   }}
   
   class Product extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleUpVote = this.handleUpVote.bind(this);
+      }
+    handleUpVote() {
+      this.props.onVote(this.props.id);
+      }
     render() {
       return (
         <div className='item'>
@@ -28,7 +43,7 @@ class ProductList extends React.Component {
           </div>
           <div className='middle aligned content'>
             <div className='header'>
-              <a>
+            <a onClick={this.handleUpVote}>
                 <i className='large caret up icon' />
               </a>
               {this.props.votes}
