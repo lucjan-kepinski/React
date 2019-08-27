30166bbac392579410519fda531d542f51acfb8b
Lucjan <lucjan.kepinski@gmail.com>
2019-08-27 20:05:26 +0200

    voting app refactored - arrow functions instead of constructors

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,20 +1,14 @@
 
 class ProductList extends React.Component {
-    constructor(props) {
-      super(props);
-      
-      this.state = {
+    state = {
       products: [],
     };
-
-    this.handleProductUpVote = this.handleProductUpVote.bind(this)
-    }
 
     componentDidMount() {
       this.setState({ products: Seed.products });
     }
 
-    handleProductUpVote(productId) {
+    handleProductUpVote = (productId) => {
       const nextProducts = this.state.products.map((product) => {
         if (product.id === productId) {
         return Object.assign({}, product, {
@@ -55,12 +49,8 @@ class ProductList extends React.Component {
   }}
   
   class Product extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleUpVote = this.handleUpVote.bind(this);
-    }
 
-    handleUpVote() {
+    handleUpVote = () => {
       this.props.onVote(this.props.id);
     }
 
