1c4ccedb1ad0b43e37f23f11bf6af60dfb0d503c
Lucjan <lucjan.kepinski@gmail.com>
2019-09-24 15:54:11 +0200

    canceling form added

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,4 +1,23 @@
 class TimersDashboard extends React.Component {
+  state = {
+    timers: [
+      {
+        title: "Practice squat",
+        project: "Gym Chores",
+        id: uuid.v4(),
+        elapsed: 5456099,
+        runningSince: Date.now()
+      },
+      {
+        title: "Bake squash",
+        project: "Kitchen Chores",
+        id: uuid.v4(),
+        elapsed: 1273998,
+        runningSince: null
+      }
+    ]
+  };
+
   handleCreateFormSubmit = timer => {
     this.createTimer(timer);
   };
@@ -28,30 +47,14 @@ class TimersDashboard extends React.Component {
     });
   };
 
-  state = {
-    timers: [
-      {
-        title: "Practice squat",
-        project: "Gym Chores",
-        id: uuid.v4(),
-        elapsed: 5456099,
-        runningSince: Date.now()
-      },
-      {
-        title: "Bake squash",
-        project: "Kitchen Chores",
-        id: uuid.v4(),
-        elapsed: 1273998,
-        runningSince: null
-      }
-    ]
-  };
-
   render() {
     return (
       <div className="ui three column centered grid">
         <div className="column">
-          <EditableTimerList timers={this.state.timers} />
+          <EditableTimerList
+            timers={this.state.timers}
+            onFormSubmit={this.handleEditFormSubmit}
+          />
           <ToggleableTimerForm onFormSubmit={this.handleCreateFormSubmit} />
         </div>
       </div>
@@ -77,6 +80,10 @@ class EditableTimerList extends React.Component {
 }
 
 class EditableTimer extends React.Component {
+  state = {
+    editFormOpen: false
+  };
+  
   handleEditClick = () => {
     this.openForm();
   };
@@ -92,9 +99,6 @@ class EditableTimer extends React.Component {
   };
   openForm = () => {
     this.setState({ editFormOpen: true });
-  };
-  state = {
-    editFormOpen: false
   };
 
   render() {
